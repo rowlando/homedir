dcd08f21d0f52bf4c882e9e93e5dcec827e6cb3a
Steve Marshall <steve@nascentguruism.com>
2013-02-06 23:23:05 +0000

    Move bootstrap stuff somewhere more apt.

--- cookbooks/nascent_workstation/recipes/basics.rb
+++
@@ -1,4 +0,0 @@
-include_recipe "pivotal_workstation::adium"
-include_recipe "pivotal_workstation::dropbox"
-include_recipe "pivotal_workstation::textmate"
-include_recipe "pivotal_workstation::things"
